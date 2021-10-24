fb6772539aace128628b7cbae9cac1a261ffb2a0
Diego Guzmán Medrano <guzmanmedrano.diego@gmail.com>
2021-10-24 18:32:59 +0200

    Añadida función que genera bloques borde y los pinta al final del tablero. Añadida funcion que borra el tetromino del tablero Gestionado el espacio y añadidos comentarios para mejor lectura

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const BOARD_HEIGHT = 20;
 let gameBoard = document.querySelector('.game-board__container');
 let miniBoard = document.querySelector('.mini-board__container__block');
 const width = 10;
-let arrayBoard = [];
+
 //función que genera los bloques individuales de ambos tableros. Cada bloque (blockDOM) tiene un bloque dentro de él (insideblockDOM). 
 //Esta función devuelve blockDOM para poder utilizarla en la función drawBoard()
 
@@ -14,7 +14,7 @@ function generateBoardBlock() {
     blockDOM.classList.add('block');
     insideblockDOM.classList.add('insideblock');
     blockDOM.appendChild(insideblockDOM);
-    makeBlock = blockDOM;
+    // makeBlock = blockDOM;------------------------------------> ¿QUÉ ES ESTO?
     return blockDOM;
 }
 
@@ -28,13 +28,41 @@ function drawBoard(boardclass, width, heigth) {
 drawBoard(gameBoard, BOARD_WIDTH, BOARD_HEIGHT);
 drawBoard(miniBoard, 4, 4);
 
+//función que genera bloques-borde
+function generateBorderBlock() {
+    const borderBlockDOM = document.createElement('div');
+    borderBlockDOM.classList.add('border__block');
+    return borderBlockDOM;
+}
 
-//Tetrominos 
+//función que pinta bloques-borde al final del tablero
+function drawBorderBlock() {
+    for (let i = 0; i < width; i++) {
+        const printBorderBlock = generateBorderBlock();
+        gameBoard.appendChild(printBorderBlock);
+    }
+}
+drawBorderBlock();
 
-const boardBlocks = gameBoard.querySelectorAll('.block'); //Selección de todos los divs dentro de gameBoard
+//---------------------------------------------------------------TETROMINOE---------------------------------------------------------------------------- 
+
+//Selección de todos los divs dentro de gameBoard
 //Array.from genera un array usando el objeto devuelto por querySelectorAll(). De esta forma podemos iterar sobre el array usando el index.
 //Al asignar una variable width a 10, podemos usar width como coordenada para iterar sobre las filas.
+
+//---------------------------------------ARRAYS--------------------------------------
+
+let arrayBoard = [];
+let arrayMiniBoard = [];
+
+const boardBlocks = gameBoard.querySelectorAll('.block');
 arrayBoard = Array.from(boardBlocks);
+
+const miniBoardBlocks = miniBoard.querySelectorAll('.block');
+arrayMiniBoard = Array.from(miniBoardBlocks);
+
+//-----------------------------------------------------------------------------------
+
 let currentPosition = 0;
 
 Itetromino = [
@@ -73,10 +101,10 @@ Jtetromino = [
 ]
 
 Otetromino = [
-    [0,1,width, width+1], //Creo que esta versión del tetromino O es mejor porque no deja espacios en blaco a los lados
-    [0,1,width, width+1],,
-    [0,1,width, width+1],,
-    [0,1,width, width+1],
+    [0, 1, width, width + 1], //Creo que esta versión del tetromino O es mejor porque no deja espacios en blaco a los lados
+    [0, 1, width, width + 1], ,
+    [0, 1, width, width + 1], ,
+    [0, 1, width, width + 1],
 ]
 
 // Otetromino = [
@@ -94,24 +122,26 @@ Ttetromino = [
 ]
 
 const allTetrominos = [Itetromino, Ltetromino, Stetromino, Ztetromino, Jtetromino, Otetromino, Ttetromino];
-    
 
-function drawTetromino() {
+
+// Pinta tetromino random al inicio del tablero
+function drawTetrominoeInMainBoard() {
     generateRandomTetrominoe().forEach(index => {
         arrayBoard[currentPosition + index].classList.add('board__tetromino');
     })
 }
+//borra el tetromino del tablero
+function undrawTetrominoeInMainBoard() {
+    generateRandomTetrominoe().forEach(index => {
+        arrayBoard[currentPosition + index].classList.remove('board__tetromino');
+    })
+}
 
-
- 
 //Obtención de una pieza de manera aleatorio, con rotacion incial
 
-function generateRandomTetrominoe(){ 
-  let randomTetrominoe = Math.floor(Math.random() * 7);
-  let chosenTetrominoe = allTetrominos[randomTetrominoe][0];
-  return chosenTetrominoe;
-
-
+function generateRandomTetrominoe() {
+    let randomTetrominoe = Math.floor(Math.random() * 7);
+    let chosenTetrominoe = allTetrominos[randomTetrominoe][0];
+    return chosenTetrominoe;
 }
-drawTetromino();
-
+drawTetrominoeInMainBoard();
